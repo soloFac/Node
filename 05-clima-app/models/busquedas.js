d9b6a4c10cda0855ea8f4146b87c2404ab28a0d4
soloFac <francopldr@gmail.com>
2023-02-22 02:59:54 -0300

    Mapbox Search API y Token de acceso

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -10,8 +10,14 @@ class Busquedas {
   async ciudad( lugar = '' ) {
 
     // peticion http
-    const resp = await axios.get('https://reqres.in/api/users?page=2')
-    console.log(resp.data.per_page)
+    try {
+      const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Otaw.json?language=es&access_token=pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA&limit=5')
+      console.log(resp.data)
+      return []
+    } catch (error) {
+      console.log(error)
+      return []
+    }
 
     return []; // retornar los lugares que coincidan
   }
