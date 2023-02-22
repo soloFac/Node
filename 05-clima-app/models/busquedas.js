ceb0991e21e3b1006df6648dcf2fbadc83458e9f
soloFac <francopldr@gmail.com>
2023-02-22 03:07:42 -0300

    Instancias de Axios

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -7,11 +7,25 @@ class Busquedas {
     // TODO: leer DB si existe
   }
 
+  get paramsMapbox() {
+    return {
+      'access_token': 'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA',
+      'language': 'es',
+      'limit': 5
+    }
+  }
+
   async ciudad( lugar = '' ) {
 
-    // peticion http
     try {
-      const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Otaw.json?language=es&access_token=pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA&limit=5')
+      // peticion http
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+        params: this.paramsMapbox
+      })
+
+      const resp = await instance.get()
+
       console.log(resp.data)
       return []
     } catch (error) {
