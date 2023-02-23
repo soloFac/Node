4c670812ba0a3110623751d6c3a2da8c53205e64
soloFac <francopldr@gmail.com>
2023-02-23 01:27:18 -0300

    Servir un sitio web completo

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -6,8 +6,12 @@ const port = 8080;
 // Servir contenido estatico
 app.use( express.static('public') );
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hello mundo en su respectiva ruta')
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
 })
 
 app.get('*', (req, res) => {
