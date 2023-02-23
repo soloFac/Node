0a61fc6891aa784d7c033d3643642037a9280d21
soloFac <francopldr@gmail.com>
2023-02-23 01:42:50 -0300

    Argumentos desde el controlador

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -11,7 +11,10 @@ app.set('view engine', 'hbs');
 app.use( express.static('public') );
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', {
+    nombre: 'Franco de la Rosa',
+    titulo: 'Curso de Node'
+  })
 })
 
 app.get('/generic', (req, res) => {
@@ -25,7 +28,6 @@ app.get('/elements', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
