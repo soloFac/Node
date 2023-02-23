c2c9003b2d87036db2565a27bdaf4cfba00ab8e1
soloFac <francopldr@gmail.com>
2023-02-23 01:38:29 -0300

    Handlebars

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+// Todo: require('hbs')
+// Renderizar con hbs - La idea de hbs es utilizar el patron MVC
+app.set('view engine', 'hbs');
+
 // midleware para hacer publica la carpeta public, con index.html
 // Servir contenido estatico
 app.use( express.static('public') );
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
 
 app.get('/generic', (req, res) => {
   res.sendFile(__dirname + '/public/generic.html')
