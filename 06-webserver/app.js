ce65f6f5d026ab916bb47152ef0ff349950bab67
soloFac <francopldr@gmail.com>
2023-02-22 22:03:30 -0300

    Request y Response

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -4,7 +4,20 @@ const http = require('http')
 // response es lo que va a responder al cliente
 http.createServer( ( req, res ) => {
 
-  res.write('Hola Mundo')
+  // res.writeHead(200, { 'Content-Type': 'aplication/json'})
+  res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+  res.writeHead(200, { 'Content-Type': 'aplication/csv'})
+
+  const persona = {
+    id: 1,
+    nombre: 'Franco'
+  }
+
+  // res.write( JSON.stringify( persona ) )
+  res.write( 'id, nombre\n' )
+  res.write( '1, Fernando\n' )
+  res.write( '2, Maria\n' )
+  res.write( '3, Jose\n' )
   res.end()
 
 })
