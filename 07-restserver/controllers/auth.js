3ef12ef964b483f3031942ce2a6fb89c320f2d99
soloFac <francopldr@gmail.com>
2023-05-12 22:25:35 -0300

    Ruta para manejar la autenticación de Google

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -43,6 +43,16 @@ const login = async ( req, res = response ) => {
   }
 }
 
+const googleSignIn = async ( req, res = response ) => {
+  const { id_token } = req.body;
+
+  res.json({
+    msg: 'Todo bien!',
+    id_token
+  })
+}
+
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
