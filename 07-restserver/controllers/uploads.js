9f76e3aa20344b9424bb52c5fa55cd46530ce9b2
soloFac <francopldr@gmail.com>
2023-07-23 19:37:36 -0300

    Subir archivos

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,7 +1,25 @@
+const path = require( 'node:path' )
+
 const { response } = require( 'express' )
 
 const cargarArchivo = ( req, res = response ) => {
-  // if ( !req.files )
+  if ( !req.files || Object.keys( req.files ).length === 0 || !req.files.archivo ) {
+    res.status( 400 ).json( { msg: 'No hay archivos que subir' } )
+    return
+  }
+
+  console.log( 'req.files >>>', req.files )
+
+  const { archivo } = req.files
+
+  const uploadPath = path.join( __dirname, '../uploads/', archivo.name )
+
+  archivo.mv( uploadPath, ( err ) => {
+    if ( err ) {
+      return res.status( 500 ).json( { err } )
+    }
+    res.json( 'File puloaded to ' + uploadPath )
+  } )
 }
 
 module.exports = {
