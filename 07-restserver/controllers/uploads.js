719e55b1a9288dbb298fec87c7578b1bcbb392fe
soloFac <francopldr@gmail.com>
2023-07-24 20:10:47 -0300

    Validar la extension

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -2,6 +2,16 @@ const path = require( 'node:path' )
 
 const { response } = require( 'express' )
 
+const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif']
+
+const validarExtension = ( ext ) => {
+  // path.basename('/tmp/midu-secre-files/password.txt') // - Nombre del fichero -> password
+  if ( extensionesValidas.includes( ext ) ) {
+    return true
+  }
+  return false
+}
+
 const cargarArchivo = ( req, res = response ) => {
   if ( !req.files || Object.keys( req.files ).length === 0 || !req.files.archivo ) {
     res.status( 400 ).json( { msg: 'No hay archivos que subir' } )
@@ -12,14 +22,24 @@ const cargarArchivo = ( req, res = response ) => {
 
   const { archivo } = req.files
 
-  const uploadPath = path.join( __dirname, '../uploads/', archivo.name )
+  const ext = path.extname( archivo.name ).slice( 1 )
 
-  archivo.mv( uploadPath, ( err ) => {
-    if ( err ) {
-      return res.status( 500 ).json( { err } )
-    }
-    res.json( 'File puloaded to ' + uploadPath )
-  } )
+  if ( !validarExtension( ext ) ) {
+    return res.status( 400 ).json( {
+      msg: `La extensión ${ ext } no es permitida, ${ extensionesValidas }`
+    } )
+  }
+
+  res.json( { ext } )
+
+  // const uploadPath = path.join( __dirname, '../uploads/', archivo.name )
+
+  // archivo.mv( uploadPath, ( err ) => {
+  //   if ( err ) {
+  //     return res.status( 500 ).json( { err } )
+  //   }
+  //   res.json( 'File puloaded to ' + uploadPath )
+  // } )
 }
 
 module.exports = {
