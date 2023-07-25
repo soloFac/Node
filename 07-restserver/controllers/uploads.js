563369aee754916e3864f4449496cdd18e58b094
soloFac <francopldr@gmail.com>
2023-07-24 21:22:57 -0300

    Helper - SubirArchivo

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,20 +1,9 @@
-const path = require( 'node:path' )
-
-const { v4: uuidv4 } = require( 'uuid' )
-
 const { response } = require( 'express' )
+const { subirArchivo } = require( '../helpers/subirArchivo' )
 
 const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif']
 
-const validarExtension = ( ext ) => {
-  // path.basename('/tmp/midu-secre-files/password.txt') // - Nombre del fichero -> password
-  if ( extensionesValidas.includes( ext ) ) {
-    return true
-  }
-  return false
-}
-
-const cargarArchivo = ( req, res = response ) => {
+const cargarArchivo = async ( req, res = response ) => {
   if ( !req.files || Object.keys( req.files ).length === 0 || !req.files.archivo ) {
     res.status( 400 ).json( { msg: 'No hay archivos que subir' } )
     return
@@ -22,25 +11,10 @@ const cargarArchivo = ( req, res = response ) => {
 
   console.log( 'req.files >>>', req.files )
 
-  const { archivo } = req.files
+  const nombre = await subirArchivo( req.files, extensionesValidas )
 
-  const ext = path.extname( archivo.name ).slice( 1 )
-
-  if ( !validarExtension( ext ) ) {
-    return res.status( 400 ).json( {
-      msg: `La extensión ${ ext } no es permitida, ${ extensionesValidas }`
-    } )
-  }
-
-  const nombreTemp = uuidv4() + '.' + ext
-
-  const uploadPath = path.join( __dirname, '../uploads/', nombreTemp )
-
-  archivo.mv( uploadPath, ( err ) => {
-    if ( err ) {
-      return res.status( 500 ).json( { err } )
-    }
-    res.json( 'File puloaded to ' + uploadPath )
+  res.json( {
+    nombre
   } )
 }
 
---
+++ 07-restserver/helpers/subirArchivo.js
@@ -0,0 +1,28 @@
+const path = require( 'node:path' )
+const { v4: uuidv4 } = require( 'uuid' )
+
+const subirArchivo = ( files, extensionesValidas, carpeta = '' ) => {
+  return new Promise( ( resolve, reject ) => {
+    const { archivo } = files
+
+    const ext = path.extname( archivo.name ).slice( 1 )
+
+    if ( !extensionesValidas.includes( ext ) ) {
+      return reject( new Error( `La extensión ${ ext } no es permitida, ${ extensionesValidas }` ) )
+    }
+
+    const nombreTemp = uuidv4() + '.' + ext
+    const uploadPath = path.join( __dirname, '../uploads/', carpeta, nombreTemp )
+
+    archivo.mv( uploadPath, ( err ) => {
+      if ( err ) {
+        return reject( new Error( { err } ) )
+      }
+      resolve( nombreTemp )
+    } )
+  } )
+}
+
+module.exports = {
+  subirArchivo
+}
