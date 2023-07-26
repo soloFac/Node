7dd9ad9ac99c86835ebedb98827f77afe06545d5
soloFac <francopldr@gmail.com>
2023-07-26 17:11:00 -0300

    Actualizar imagen de Usuario y Producto

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,5 +1,6 @@
 const { response } = require( 'express' )
 const { subirArchivo } = require( '../helpers/subirArchivo' )
+const { Usuario, Producto } = require( '../models' )
 
 const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif']
 
@@ -27,10 +28,35 @@ const cargarArchivo = async ( req, res = response ) => {
 const actualizarImagen = async ( req, res = response ) => {
   const { id, coleccion } = req.params
 
-  res.json( {
-    id,
-    coleccion
-  } )
+  let modelo
+
+  switch ( coleccion ) {
+  case 'usuarios':
+    modelo = await Usuario.findById( id )
+    if ( !modelo ) {
+      return res.status( 400 ).json( {
+        msg: `No existe un usuario con el id ${ id }`
+      } )
+    }
+    break
+  case 'productos':
+    modelo = await Producto.findById( id )
+    if ( !modelo ) {
+      return res.status( 400 ).json( {
+        msg: `No existe un producto con el id ${ id }`
+      } )
+    }
+    break
+  default:
+    return res.status( 500 ).json( { msg: 'Se me olvidó validar esto' } )
+  }
+
+  const nombre = await subirArchivo( req.files, extensionesValidas, coleccion )
+  modelo.img = nombre
+
+  await modelo.save()
+
+  res.json( { modelo } )
 }
 
 module.exports = {
