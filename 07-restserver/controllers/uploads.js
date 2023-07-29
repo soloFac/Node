4c711aa899edac43d755d63653087f6c5cde26f7
soloFac <francopldr@gmail.com>
2023-07-29 16:47:54 -0300

    Resolución de tarea - Validar archivo a subir

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -5,13 +5,6 @@ const { Usuario, Producto } = require( '../models' )
 const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif']
 
 const cargarArchivo = async ( req, res = response ) => {
-  if ( !req.files || Object.keys( req.files ).length === 0 || !req.files.archivo ) {
-    res.status( 400 ).json( { msg: 'No hay archivos que subir' } )
-    return
-  }
-
-  console.log( 'req.files >>>', req.files )
-
   try {
     const nombre = await subirArchivo( req.files, extensionesValidas, 'imgs' )
 
--- 07-restserver/routes/uploads.js
+++ 07-restserver/routes/uploads.js
@@ -4,12 +4,14 @@ const { check } = require( 'express-validator' )
 const { validarCampos } = require( '../middlewares/validar-campos' )
 const { cargarArchivo, actualizarImagen } = require( '../controllers/uploads' )
 const { coleccionesPermitidas } = require( '../helpers/db-validators' )
+const { validarArchivoSubir } = require( '../middlewares' )
 
 const router = Router()
 
-router.post( '/', cargarArchivo )
+router.post( '/', validarArchivoSubir, cargarArchivo )
 
 router.put( '/:coleccion/:id', [
+  validarArchivoSubir,
   check( 'id', 'El id debe ser de mongo' ).isMongoId(),
   check( 'coleccion' ).custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'] ) ),
   validarCampos
