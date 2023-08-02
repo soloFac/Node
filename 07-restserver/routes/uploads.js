9b1504ed44b6e188d9faffca4c5835a6a8650e84
soloFac <francopldr@gmail.com>
2023-08-02 11:48:56 -0300

    Servicio par amostrar las imágenes

--- 07-restserver/routes/uploads.js
+++ 07-restserver/routes/uploads.js
@@ -2,7 +2,7 @@ const { Router } = require( 'express' )
 const { check } = require( 'express-validator' )
 
 const { validarCampos } = require( '../middlewares/validar-campos' )
-const { cargarArchivo, actualizarImagen } = require( '../controllers/uploads' )
+const { cargarArchivo, actualizarImagen, mostrarImagen } = require( '../controllers/uploads' )
 const { coleccionesPermitidas } = require( '../helpers/db-validators' )
 const { validarArchivoSubir } = require( '../middlewares' )
 
@@ -16,5 +16,11 @@ router.put( '/:coleccion/:id', [
   check( 'coleccion' ).custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'] ) ),
   validarCampos
 ], actualizarImagen )
+
+router.get( '/:coleccion/:id', [
+  check( 'id', 'El id debe ser de mongo' ).isMongoId(),
+  check( 'coleccion' ).custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'] ) ),
+  validarCampos
+], mostrarImagen )
 
 module.exports = router
