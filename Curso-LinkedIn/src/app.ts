def13b08bc3396eb9ee7f85121d0b4f46c39aef0
soloFac <francopldr@gmail.com>
2023-12-21 04:06:11 -0300

    Implementación de configuración de mongo

--- Curso-LinkedIn/src/app.ts
+++ Curso-LinkedIn/src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config"
+import { MongoDatabase } from "./data/mongodb"
 import { AppRoutes } from "./presentation/routes"
 import { Server } from "./presentation/server"
 
@@ -7,9 +8,11 @@ import { Server } from "./presentation/server"
 })()
 
 async function main () {
-	// todo: await base de datos
-	
-	// todo: inicio de nuestro server
+	await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL
+  })
+ 
 	new Server({
     port: envs.PORT,
     routes: AppRoutes.routes
